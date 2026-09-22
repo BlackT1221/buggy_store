3f49167db61ea046cc31628e47e96f3a2230e1b0
Aslhyy <aslhynicol0936@gmail.com>
2026-09-22 14:03:55 -0500

    Corregir el argumento predeterminado del inventario compartido

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class TiendaOnline:
     # Sistema básico de gestión de inventario y ventas
     
-    def __init__(self, inventario_inicial={}):
-        self.inventario = inventario_inicial
+    def __init__(self, inventario_inicial=None):
+        self.inventario = inventario_inicial if inventario_inicial is not None else {}
         self.ventas_totales = 0.0
 
     def agregar_producto(self, id_producto, nombre, precio, cantidad):
