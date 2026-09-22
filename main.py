c61a59a359b24732d510cb3efd7f95c9732f9afa
juan <juanstebanriveros@gmail.com>
2026-09-22 13:56:11 -0500

    fix(bug04): evita RuntimeError al limpiar agotados

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class TiendaOnline:
 
     def limpiar_agotados(self):
         """Elimina del inventario los productos con cantidad 0 o menor."""
-        for id_producto in self.inventario.keys():
+        for id_producto in list(self.inventario.keys()):
             if self.inventario[id_producto]['cantidad'] <= 0:
                 del self.inventario[id_producto]
 
