0370814313032ebe163c2818fe6955b01c938115
Oskitar.js <oscartquintero23@gmail.com>
2026-09-23 14:37:49 -0500

    fix: corrige typo en ventas_totales (I mayúscula por l)

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ class TiendaOnline:
             total_pedido = total_pedido * 1.20
 
         # Registrar la venta
-        self.ventas_totaIes += total_pedido 
+                # Registrar la venta
+        self.ventas_totales += total_pedido
         
         return total_pedido
 
