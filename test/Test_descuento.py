68585c3c724173b880b587c45eff67506ee8029d
roderick correa <adrian.cb.soft1019@gmail.com>
2026-09-23 16:16:02 -0500

    Feat: integracion del pytest del descuento

---
+++ test/Test_descuento.py
@@ -0,0 +1,17 @@
+from main import TiendaOnline
+
+def test_descuento_cupon_sena2026():
+    tienda = TiendaOnline()
+    tienda.agregar_producto("A1", "Camiseta", 100.0, 10)
+    carrito = [{"id_producto": "A1", "cantidad": 1}]
+    tienda.procesar_pedido(carrito, cupon_descuento="SENA2026")
+    assert tienda.ventas_totales == 80.0
+
+def test_sin_cupon_no_aplica_descuento():
+    tienda = TiendaOnline()
+    tienda.agregar_producto("A1", "Camiseta", 100.0, 10)
+    carrito = [{"id_producto": "A1", "cantidad": 1}]
+ 
+    total = tienda.procesar_pedido(carrito)
+ 
+    assert total == 100.0
