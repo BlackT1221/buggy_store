d20af21fad17798e2ab4ef4b3e5f7bfb6dd28e0e
cristhianpa22 <cristhianp.soft@gmail.com>
2026-09-23 15:23:37 -0500

    refactor: se refactorizo el inventario comprobando que  si la tienda se crea sin inventario quede vacio el inventario

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import pytest
 from main import TiendaOnline
 
-def ErrorSintaxis():
+def test_ErrorSintaxis():
     tienda = TiendaOnline()
     tienda.agregar_producto("P01", "Teclado Mecánico", 150000, 5)
 
@@ -12,3 +12,13 @@ def ErrorSintaxis():
 
     with pytest.raises(AttributeError, match="ventas_totaIes"):
         tienda.procesar_pedido(carrito)
+
+
+def test_Inventario_junto():
+    tienda1 = TiendaOnline()
+    tienda1.agregar_producto("P01", "Teclado", 150000, 5)
+    
+    tienda2 = TiendaOnline()
+    assert "P02" not in tienda2.inventario, f"¡Error detectado! tienda2 tiene este inventario: {tienda2.inventario}"
+    
+
