a93175402f70888a815107fa857dfeabd12455f9
cristhianpa22 <cristhianp.soft@gmail.com>
2026-09-23 15:45:15 -0500

    refactor: se refactorizo el descauanto para que si descuente el 20% y no aumente el valor en un 20%

--- tests.py
+++ tests.py
@@ -19,6 +19,13 @@ def test_Inventario_junto():
     tienda1.agregar_producto("P01", "Teclado", 150000, 5)
     
     tienda2 = TiendaOnline()
-    assert "P02" not in tienda2.inventario, f"¡Error detectado! tienda2 tiene este inventario: {tienda2.inventario}"
+    assert "P02" not in tienda2.inventario, f"tienda2 tiene este inventario: {tienda2.inventario}"
     
 
+def test_procesar_pedido_descuento_20():
+    
+    tienda = TiendaOnline()
+    tienda.agregar_producto("P01", "Teclado", 100000, 5)
+    carrito = [{'id_producto': 'P01', 'cantidad': 1}]
+    total = tienda.procesar_pedido(carrito, cupon_descuento="SENA2026")
+    assert total == 80000, f"se esperaba 80000, pero el sistema cobró {total}"
